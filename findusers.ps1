12124dd62c2242dd7e4fa2f03a59ea0dabdbe346
Jonathan Krausfeldt <61332412+krausware@users.noreply.github.com>
2021-09-17 14:23:51 -0500

    Add files via upload

---
+++ findusers.ps1
@@ -0,0 +1,23 @@
+﻿$DPTMShosts = Import-CSV C:\temp\hosts.csv
+$KMTCusers = Import-CSV C:\temp\_Fort-Campbell-KMTC-personal-cell-civilian-email-roster-11Jun2020.csv
+
+foreach($DPTMShost in $DPTMShosts)
+{
+    #tolowercase
+    echo $DPTMShost.name
+
+
+
+} a
+
+#try this
+cd \\$DPTMShost\c$\Users\ 
+#then do this
+$testUser = (gci | sort LastWriteTime | select -last 1)
+
+#test if user is in KMTC directory
+if ($testUser -in $KMTCusers.name)
+    {
+        #append outfile with the hostname and the username
+        
+    }
---
+++ string_manip.ps1
@@ -0,0 +1,36 @@
+﻿$DPTMShosts = Import-CSV C:\temp\hosts.csv
+$KMTCusers = Import-CSV C:\temp\_Fort-Campbell-KMTC-personal-cell-civilian-email-roster-11Jun2020.csv
+$KMTCnames = $KMTCusers.name.ToLower()
+
+foreach ($KMTCname in $KMTCnames)
+{
+       #ignore up to comma the capture
+      $fName =  ($KMTCname | Select-String -Pattern ', ' )
+      echo $fname
+
+       #capture up to comma then ignore
+      #$KMTCname = $KMTCname -replace '[,]',''
+      #echo $KMTCname
+}
+
+
+#foreach($DPTMShost in $DPTMShosts)
+#{
+    #tolowercase
+    #echo $DPTMShost.name
+
+
+
+#} 
+
+
+
+#echo $KMTCnames
+#foreach($KMTCuser in $KMTCusers)
+#{
+    #tolowercase
+#    echo $KMTCuser.name
+
+
+
+#} 
